de61e5b69d0865ce04e97ac51f89440ee8dc1489
krishhackz <krishhackz.in@gmail.com>
2024-10-28 19:26:45 +0530

    Providing user input

--- Pattern/NumberSquare.c
+++ Pattern/NumberSquare.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 void main(){
-int i,j;
-for(i=0;i<4;i++){
+int user,i,j;
+printf("Enter a number: ");
+scanf("%d",&user);
+for(i=0;i<user;i++){
     int a=1;
-    for(j=0;j<4;j++){
+    for(j=0;j<user;j++){
         printf("%d",a);
         a++;
     }
