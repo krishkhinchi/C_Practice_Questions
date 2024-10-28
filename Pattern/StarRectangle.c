c82dd2386edc6453a42f18f68b7c001e8b2bb3eb
krishhackz <krishhackz.in@gmail.com>
2024-10-28 09:06:01 +0530

    Improving star rectangle pattern

--- Pattern/StarRectangle.c
+++ Pattern/StarRectangle.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main (){
     int i,j;
-    for(i=0;i<10;i++){
-        for(j=0;j<5;i++){
+    for(i=0;i<3;i++){
+        for(j=0;j<15;j++){
             printf("*");
         }  
         printf("\n");
