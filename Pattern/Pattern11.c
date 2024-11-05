678666d0f28654b2cddf5fa50df0b00fcc57023f
krishhackz <krishhackz.in@gmail.com>
2024-11-05 15:19:40 +0530

    Horizontal Alphabet Square, adding user input

--- Pattern/Pattern11.c
+++ Pattern/Pattern11.c
@@ -1,5 +1,5 @@
 /*
-Horizontal Alphabet Square
+Horizontal Alphabet Square #1
 
 ABCD
 ABCD
@@ -10,11 +10,14 @@ ABCD
 
 #include <stdio.h>
 int main() {
-    int i;
-    char j;
-    for(i=0;i<4;i++){
-        for(j='A';j<='D';j++){
-            printf("%c",j);
+    int user_input,i,j;
+    printf("Enter a number: ");
+    scanf("%d",&user_input);
+    for(i=0;i<user_input;i++){
+        int a='A';
+        for(j=0;j<user_input;j++){
+            printf("%c",a);
+            a++;
         }
         printf("\n");
     }
