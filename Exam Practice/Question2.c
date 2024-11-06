6b3707bd940fe5d93091f9ae84fb0ce23ef1c548
krishhackz <krishhackz.in@gmail.com>
2024-11-06 14:49:41 +0530

    Write a C program to display Your Name, Address and City in different lines

---
+++ Exam Practice/Question2.c
@@ -0,0 +1,10 @@
+//Write a C program to display Your Name, Address and City in different lines.
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+printf("Name: Chikoo Choudhury\n");
+printf("Address: 123 Chuckle Lane, Giggly Gardens, Ahmedabad, 380001\n");
+printf("City: Ahmedabad, Gujarat");
+getch();
+}
