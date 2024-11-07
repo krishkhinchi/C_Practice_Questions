c48fe245011eb369f028b78377ec13e7cd02d425
krishhackz <krishhackz.in@gmail.com>
2024-11-07 14:08:29 +0530

    Write a program to find a minimum from given two numbers

--- Exam Practice/Questions7.c
+++ Exam Practice/Questions7.c
@@ -4,5 +4,18 @@
 #include<conio.h>
 void main(){
 clrscr();
+int a,b;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+
+if(a>b){
+    printf("Minimum number is %d",b);
+} else if(b>a){
+    printf("Minimum number is %d",a);
+} else{
+    printf("Both numbers are equal");
+}
 getch();
 }
