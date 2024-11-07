a2e3b97064a378249c761e6248d6ee4de7f0e1d9
krishhackz <krishhackz.in@gmail.com>
2024-11-07 21:12:55 +0530

    Write a C program to print the following shape. * * * * * * * * * *

---
+++ Exam Practice/Question19.c
@@ -0,0 +1,13 @@
+// Write a C program to print the following shape. * * * * * * * * * *
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+
+for(int i=0;i<10;i++){
+    printf("* ");
+}
+
+getch();
+}
