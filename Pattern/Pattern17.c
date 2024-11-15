eda9702b47626cb371f395e18b92820e9dfb2726
krishhackz <krishhackz.in@gmail.com>
2024-11-15 13:26:12 +0530

    Star Plus Pattern #1, Solving minor bug at line 17

--- Pattern/Pattern17.c
+++ Pattern/Pattern17.c
@@ -1,5 +1,5 @@
 /*
-Star Plus Pattern
+Star Plus Pattern #1
 
   *
   *
@@ -14,7 +14,7 @@ Star Plus Pattern
 void main(){
 clrscr();
 int k,i,j;
-for(i<0;i<5;i++){
+for(i=0;i<5;i++){
     if(!(i==2)){
         for(j=0;j<5;j++){
             if(!(j==2)){
