ca855daa2f981d5036600ab9dab6a25e0c40e034
krishhackz <krishhackz.in@gmail.com>
2024-11-15 10:24:36 +0530

    Star Plus Pattern

---
+++ Pattern/Pattern17.c
@@ -0,0 +1,33 @@
+/*
+Star Plus Pattern
+
+  *
+  *
+*****
+  * 
+  * 
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+int k,i,j;
+for(i<0;i<5;i++){
+    if(!(i==2)){
+        for(j=0;j<5;j++){
+            if(!(j==2)){
+                printf(" ");
+            } else{
+                printf("*");
+            }
+        }        
+    } else{
+        for(j=0;j<5;j++){
+            printf("*");
+        } 
+    } printf("\n");
+}
+getch();
+}
