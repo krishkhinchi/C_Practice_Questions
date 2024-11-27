594b8edbd79266363df9ac9ac512b8cf02c2ce51
krishhackz <krishhackz.in@gmail.com>
2024-11-27 21:49:21 +0530

    Creating a stucture on laptop

---
+++ Structure/laptop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct Laptop{
+    char Brand;
+    char Model;
+    char Color;
+    int Ram;
+    int Storage;
+    char Processer;
+    char Gpu;
+    float Display;
+    int Price;
+};
+
+void main(){
+clrscr();
+
+getch();
+}
